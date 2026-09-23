77c1657478055ac6f359c46d16f0d5a0a272f781
andgenki-sketch <andgenki@gmail.com>
2026-09-23 17:42:15 +0900

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"ライブハウスイベントマネージャー",description:"イベント制作・ブッキング管理"};
+export default function RootLayout({children}){return <html lang="ja"><body>{children}</body></html>}
